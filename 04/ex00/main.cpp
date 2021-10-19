90fe1fefa0a2b65816650d4652c5409c8abbcbea
Simon GAUBERT <sgaubert@e3r1p5.42.fr>
2021-10-19 21:46:38 +0200

    update all Makefile

--- 04/ex00/main.cpp
+++ 04/ex00/main.cpp
@@ -7,18 +7,18 @@
 int main(int argc, char const *argv[])
 {
 	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
+	const Animal* dog = new Dog();
+	const Animal* cat = new Cat();
 
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
+	std::cout << dog->getType() << std::endl;
+	std::cout << cat->getType() << std::endl;
 
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
+	cat->makeSound();
+	dog->makeSound();
 	meta->makeSound();
 
 	delete meta;
-	delete j;
-	delete i;
+	delete dog;
+	delete cat;
 	return 0;
 }
