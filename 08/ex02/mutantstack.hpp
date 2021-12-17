6d8fae3c2d202354144138cc63c5a00fd72df30b
sgaubert <sim.gaubert.sg@gmail.com>
2021-12-17 13:50:26 +0100

    update cpp08/ex02

--- 08/ex02/mutantstack.hpp
+++ 08/ex02/mutantstack.hpp
@@ -27,6 +27,15 @@ class Mutantstack : public std::stack<T> {
 
 		// Member functions
 
+		// Iterator
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+
+		iterator begin(void) { return this->c.begin(); }
+		iterator end(void) { return this->c.end(); }
+		reverse_iterator rbegin(void) { return this->c.rbegin(); }
+		reverse_iterator rend(void) { return this->c.rend(); }
+
 };
 
 #endif // MUTANTSTACK_HPP
